cc6fc3d028fb5c1ee6855f5abc40e863bcccb68b
GyoOh <goh3@my.bcit.ca>
2023-12-07 20:32:07 -0800

    updated until user in the db

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
 
 const express = require('express');
 const app = express();
-
+let db = require("./fake-db");
+const cors = require('cors');
 const port = 8000;
 
+app.use(cors({origin: 'http://localhost:3000'}));
+
 app.get('/', (req, res) => { 
-    res.send('Hello World!');
+    const users = db.getUsers();
+    res.send(users);
+});
+app.get('/user/:id', (req, res) => {
+    const user = db.getUser(req.params.id);
+    res.send(user);
 });
 
 app.listen(port, () => {
